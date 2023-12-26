df323bdaad51958a5405cc309d0a29f960bc9c21
Roman <romazelenskiy2000@gmail.com>
2023-12-26 23:26:51 +0200

    fix

--- components/ui/ButtonOpacityDelete.vue
+++ components/ui/ButtonOpacityDelete.vue
@@ -2,7 +2,7 @@
 
 <template>
   <UiButtonOpacity
-    class="w-13px h-[15px] md:w-[15px] md:h-[18px] xl:w-[19px] xl:h-[23px]"
+    class="w-[13px] h-[15px] md:w-[15px] md:h-[18px] xl:w-[19px] xl:h-[23px]"
   >
     <SvgoDelete class="text-white !w-full !h-full"></SvgoDelete>
   </UiButtonOpacity>
