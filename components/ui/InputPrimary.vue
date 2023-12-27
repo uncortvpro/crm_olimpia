a9def7d9b50df75b2ec0e9319ee30ccadc6f9895
Roman <romazelenskiy2000@gmail.com>
2023-12-27 13:47:10 +0200

    fix

--- components/ui/InputPrimary.vue
+++ components/ui/InputPrimary.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 defineProps<{
   modelValue?: string;
+  type: string;
 }>();
 
 const emits = defineEmits(["update:modelValue"]);
@@ -12,7 +13,23 @@ const handleInput = (event: Event) => {
 </script>
 
 <template>
-  <input :value="modelValue" @input="handleInput" class="input_primary_style" />
+  <UInput
+    :value="modelValue"
+    @input="handleInput"
+    v-if="type === 'password'"
+    type="password"
+    :ui="{
+      wrapper: 'input_primary_style',
+      base: 'h-full !bg-transparent !text-white !border-none focus:outline !ring-0',
+    }"
+  />
+  <input
+    v-else
+    :value="modelValue"
+    :type="type"
+    @input="handleInput"
+    class="input_primary_style"
+  />
 </template>
 
 <style scoped></style>
