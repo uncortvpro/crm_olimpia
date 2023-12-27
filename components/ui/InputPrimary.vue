47c6e628e337fe021247072b384dbac6f23b2d39
Roman <romazelenskiy2000@gmail.com>
2023-12-27 13:56:48 +0200

    fix

--- components/ui/InputPrimary.vue
+++ components/ui/InputPrimary.vue
@@ -13,18 +13,7 @@ const handleInput = (event: Event) => {
 </script>
 
 <template>
-  <UInput
-    :value="modelValue"
-    @input="handleInput"
-    v-if="type === 'password'"
-    type="password"
-    :ui="{
-      wrapper: 'input_primary_style',
-      base: 'h-full !bg-transparent !text-white !border-none focus:outline !ring-0',
-    }"
-  />
   <input
-    v-else
     :value="modelValue"
     :type="type"
     @input="handleInput"
